3c496cc12279e0c05bf91255effcedd1cf416f8f
Felipe P. <85078048+Fepaab@users.noreply.github.com>
2025-02-05 20:04:06 -0300

    Add files via upload

---
+++ senha.py
@@ -0,0 +1,6 @@
+senhacorreta = input("Defina a senha: ")
+senha = input("Digite sua senha: ")
+if senhacorreta == senha:
+    print("Você ACERTOU a senha")
+if senhacorreta != senha:
+    print("Você ERROU a senha")
